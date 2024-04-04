568a22f6457659583e7115d59765870bf3c9eb74
penroseDG <147390358+penroseDG@users.noreply.github.com>
2024-04-04 11:40:21 +0700

    Add files via upload

---
+++ Ôn tập html/bài tập 1/b2.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    let age = document.getElementById('ageInput').value;
+    alert('Số tuổi bạn vừa nhập là: ' + age);
+    console.log('Số tuổi bạn vừa nhập là: ' + age);
+    
+}
